e5df71fb871c0d550e3415af036b873aac1c4cd3
Aditi Dable <aditidable01@gmail.com>
2021-05-04 12:19:39 +0530

    added

---
+++ mern-ecommerce/ecommerce-bend/src/models/user.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      min: 3,
+      max: 20,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      min: 3,
+      max: 20,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      index: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    hash_password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin", "super-admin"],
+      default: "user",
+    },
+    contactNumber: { type: String },
+    pofilePicture: { type: String },
+  },
+  { timestamps: true }
+);
+
+userSchema.virtual('password')
+.set(function(password){
+    //this.hash_password = bcrypt.hashSync(password, 10);
+     bcrypt.hash(password, 10, function(err, hash) {
+      // Store hash in your password DB.
+      this.hash_password=hash;
+    console.log(err)
+
+  });
+});
+
+// userSchema.virtual("fullName").get(function () {
+//   return `${this.firstName} ${this.lastName}`;
+// });
+
+userSchema.methods = {
+  authenticate: function (password) {
+    return bcrypt.compare(password, this.hash_password);
+  },
+};
+
+module.exports = mongoose.model('User', userSchema);
